d6cace0f9314a4579236094bbddf53818916a9d3
Giovani M <gioaldo172@gmail.com>
2023-12-03 00:50:30 -0600

    Agregar archivos de proyecto.

---
+++ variables/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 10;
+            int numero2 = 20;
+
+            int suma = numero1 + numero2;
+
+            Console.WriteLine("la suma es: " + suma);
+        }
+    }
+}
